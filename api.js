c4c578da10a9c637829019af9c1cdeb993647ca2
William Gardner <wg4568@gmail.com>
2017-07-30 00:33:54 -0700

    comments

--- api.js
+++ api.js
@@ -1,10 +1,14 @@
+// Container for API methods
 var API = {};
 
+// URL that the API posts to
+API.URL = "http://localhost:3000";
 API.URL = "https://cg-loopback.mybluemix.net";
-// API.URL = "http://localhost:3000";
 
+// Container for request methods
 API.Request = {};
 
+// Makes GET request to API
 API.Request.Get = function(url, callback) {
 	$.ajax({
 		type: "GET",
@@ -14,6 +18,7 @@ API.Request.Get = function(url, callback) {
 	});
 }
 
+// Makes POST request to API
 API.Request.Post = function(url, data, callback) {
 	$.ajax({
 		type: "POST",
@@ -24,16 +29,20 @@ API.Request.Post = function(url, data, callback) {
 	});
 }
 
+// Container for methods relating to users
 API.Users = {};
 
+// Gets all users in database as array
 API.Users.getAll = function(callback) {
 	API.Request.Get("/api/users", callback);
 }
 
+// Gets single user with given ID
 API.Users.getById = function(id, callback) {
 	API.Request.Get("/api/users/" + id, callback);
 }
 
+// Creates a user from 'info' object
 API.Users.create = function(info, callback) {
 	API.Request.Post("/api/createuser", {
 		username: info.username,
@@ -45,12 +54,7 @@ API.Users.create = function(info, callback) {
 	}, callback);
 }
 
-API.Projects = {};
-
-API.Projects.getAll = function(callback) {
-	API.Request.Get("/api/projects", callback);
-}
-
+// Gets single project with given ID
 API.Projects.getById = function(id, callback) {
 	API.Request.Get("/api/projects/" + id, callback);
 }
